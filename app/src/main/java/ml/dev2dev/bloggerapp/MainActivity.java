ef062c18746bf6f36a39e33eb940fa8fca889e5e
Benson Mburu <nguruben@gmail.com>
2015-11-16 20:27:42 +0300

    Project Start

---
+++ app/src/main/java/ml/dev2dev/bloggerapp/MainActivity.java
@@ -0,0 +1,138 @@
+package ml.dev2dev.bloggerapp;
+
+import android.os.AsyncTask;
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.widget.ListAdapter;
+        import android.widget.ListView;
+        import android.widget.SimpleAdapter;
+        import android.widget.TextView;
+
+        import org.apache.http.HttpEntity;
+        import org.apache.http.HttpResponse;
+        import org.apache.http.client.methods.HttpPost;
+        import org.apache.http.impl.client.DefaultHttpClient;
+        import org.apache.http.params.BasicHttpParams;
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
+
+        import java.io.BufferedReader;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.util.ArrayList;
+        import java.util.HashMap;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    String myJSON;
+
+    private static final String TAG_RESULTS="result";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "Title";
+    private static final String TAG_ADD ="Blogpost";
+
+    JSONArray peoples = null;
+
+    ArrayList<HashMap<String, String>> personList;
+
+    ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.listView);
+        personList = new ArrayList<HashMap<String,String>>();
+        getData();
+    }
+
+
+    protected void showList(){
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            peoples = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for(int i=0;i<peoples.length();i++){
+                JSONObject c = peoples.getJSONObject(i);
+                String id = c.getString(TAG_ID);
+                String Title = c.getString(TAG_NAME);
+                String Blogpost = c.getString(TAG_ADD);
+
+                HashMap<String,String> persons = new HashMap<String,String>();
+
+                persons.put(TAG_ID,id);
+                persons.put(TAG_NAME,Title);
+                persons.put(TAG_ADD,Blogpost);
+
+                personList.add(persons);
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, personList, R.layout.list_items,
+                    new String[]{TAG_ID,TAG_NAME,TAG_ADD},
+                    new int[]{R.id.id, R.id.Title, R.id.Blogpost}
+            );
+
+            list.setAdapter(adapter);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getData(){
+        class GetDataJSON extends AsyncTask<String, Void, String>{
+
+            @Override
+            protected String doInBackground(String... params) {
+                DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+                HttpPost httppost = new HttpPost("http://seabanpr.co.ke/APILogin/client.php");
+
+                // Depends on your web service
+                httppost.setHeader("Content-type", "application/json");
+
+                InputStream inputStream = null;
+                String result = null;
+                try {
+                    HttpResponse response = httpclient.execute(httppost);
+                    HttpEntity entity = response.getEntity();
+
+                    inputStream = entity.getContent();
+                    // json is UTF-8 by default
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+                    StringBuilder sb = new StringBuilder();
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        sb.append(line + "\n");
+                    }
+                    result = sb.toString();
+                } catch (Exception e) {
+                    // Oops
+                }
+                finally {
+                    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result){
+                myJSON=result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute();
+    }
+
+
+
+
+}
